8fbbdc7168ba87dc7a405abfc4fd80de4aa42873
Yiyao Zhang <me@yiyao.ca>
2021-02-16 22:43:13 -0500

    database change: mysql -> sqlite3

--- mail_system_controller/mail_system_controller/settings.py
+++ mail_system_controller/mail_system_controller/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'mail_system_controller.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'amd_mailsystem',
+#         'USER': 'root',
+#         'PASSWORD': 'HxiB0vhVnWB2dAUsamX8cc',
+#         'HOST': '142.55.32.86',
+#         'PORT': '3306'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'amd_mailsystem',
-        'USER': 'root',
-        'PASSWORD': 'HxiB0vhVnWB2dAUsamX8cc',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
